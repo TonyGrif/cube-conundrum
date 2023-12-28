d4241485d3a790b589b2d669ca8819b691faf89b
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-27 21:39:58 -0500

    check for game possibility

--- tests/test_game.py
+++ tests/test_game.py
@@ -3,6 +3,13 @@ import pytest
 from src.game import Game, Turn
 
 
+@pytest.fixture
+def game():
+    return Game(
+        "Game 1: 4 red, 3 blue; 6 blue, 16 green; 9 blue, 13 green, 1 red; 10 green, 4 red, 6 blue"
+    )
+
+
 class TestGameClass:
     def test_turn(self):
         turn = Turn(3, 3, 3)
@@ -22,13 +29,18 @@ class TestGameClass:
         game = Game("Game 77:")
         assert game.identification == 77
 
-    def test_parsing(self):
-        game = Game(
-            "Game 1: 4 red, 3 blue; 6 blue, 16 green; 9 blue, 13 green, 1 red; 10 green, 4 red, 6 blue"
-        )
-
+    def test_parsing(self, game):
         assert len(game.turns) == 4
         assert game.turns[0] == Turn(4, 0, 3)
         assert game.turns[1] == Turn(0, 16, 6)
         assert game.turns[2] == Turn(1, 13, 9)
         assert game.turns[3] == Turn(4, 10, 6)
+
+    def test_possibility(self, game):
+        assert game.is_possible(20, 20, 20)
+        assert not game.is_possible(1, 1, 1)
+
+        assert not game.is_possible(3, 20, 20)
+        assert not game.is_possible(20, 15, 20)
+        assert not game.is_possible(20, 20, 8)
+        assert game.is_possible(4, 16, 9)
