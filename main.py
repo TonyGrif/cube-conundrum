3c16b9ef60ffc7058c0727df6b20afc873bb6c72
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-27 21:53:09 -0500

    add valid identifications

--- main.py
+++ main.py
@@ -59,9 +59,14 @@ def main():
         "Accepting rgb values of: %s, %s, %s", args.red, args.green, args.blue
     )
 
+    summation = 0
     with open(args.txt_file, "r", encoding="utf-8") as file:
         for line in file:
             game = Game(line)
+            if game.is_possible(args.red, args.green, args.blue):
+                summation += game.identification
+
+    print(f"The resulting summations is {summation}")
 
 
 if __name__ == "__main__":
